bd3fcb8195b7173cf8a1369be48699594e34efdf
Camilo Iral <juan.camilo@landing.ai>
2024-07-03 19:25:53 -0500

    fix2

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setuptools.setup(
     name="depth_anything_v2",
     version="0.0.1",
     python_requires=">=3.9",  
-    packages=["depth_anything_v2", "depth_anything_v2.dinov2_layers", "depth_anything_v2.utils"],
+    packages=["depth_anything_v2", "depth_anything_v2.dinov2_layers", "depth_anything_v2.util"],
     install_requires=requirements,
 )
